7d7a53e1501f19c529029ccf521ef299165a9841
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-01-28 19:22:06 -0800

    setting up start of chapter 8

---
+++ 8-session-management/README.md
@@ -0,0 +1,13 @@
+# 8. Session Management
+
+In this chapter, you will learn about session management in Remix. First, we will use the URL to store application state. Next, we will utilize cookies to manage user sessions. Finally, we will see how to authenticate users with session cookies.
+
+## Getting started
+
+Note that the code in the [start](./start/) folder of this chapter is different from our final [solution](/7-error-handling-in-remix/bee-rich/solution/) from _Chapter 7, Error Handling in Remix_.
+
+If you want to reuse the code from the previous chapter, then follow the following guide to get to the starting point of this chapter. Otherwise, feel free to checkout the code in the [start](./start/) folder and dive right into _Chapter 8, Session Management_.
+
+## Adding users to our database model
+
+Happy coding!
---
+++ 8-session-management/bee-rich/start/prisma/seed.ts
@@ -0,0 +1,179 @@
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
+
+const expenses = [
+  {
+    title: 'Groceries',
+    amount: 50,
+    currencyCode: 'USD',
+    date: '2022-12-05',
+  },
+  {
+    title: 'Gym Membership',
+    amount: 20,
+    currencyCode: 'USD',
+    date: '2022-12-03',
+  },
+  {
+    title: 'Movies',
+    amount: 20,
+    currencyCode: 'USD',
+    date: '2022-12-02',
+  },
+  {
+    title: 'Mobile Service',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-11-01',
+  },
+  {
+    title: 'Rent December',
+    amount: 1000,
+    currencyCode: 'USD',
+    date: '2022-12-01',
+  },
+  {
+    title: 'Groceries',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-12-01',
+  },
+  {
+    title: 'Takeout',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-11-30',
+  },
+  {
+    title: 'Gym Membership',
+    amount: 20,
+    currencyCode: 'USD',
+    date: '2022-11-03',
+  },
+  {
+    title: 'Groceries',
+    amount: 15,
+    currencyCode: 'USD',
+    date: '2022-11-02',
+  },
+  {
+    title: 'Mobile Service',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-11-01',
+  },
+  {
+    title: 'Rent November',
+    amount: 1000,
+    currencyCode: 'USD',
+    date: '2022-11-01',
+  },
+  {
+    title: 'Groceries',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-10-30',
+  },
+  {
+    title: 'Groceries',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-10-15',
+  },
+  {
+    title: 'Dinner',
+    amount: 40,
+    currencyCode: 'USD',
+    date: '2022-10-11',
+  },
+  {
+    title: 'Gym Membership',
+    amount: 20,
+    currencyCode: 'USD',
+    date: '2022-10-03',
+  },
+  {
+    title: 'Groceries',
+    amount: 25,
+    currencyCode: 'USD',
+    date: '2022-10-02',
+  },
+  {
+    title: 'Mobile Service',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-10-01',
+  },
+  {
+    title: 'Rent October',
+    amount: 1000,
+    currencyCode: 'USD',
+    date: '2022-10-01',
+  },
+  {
+    title: 'Groceries',
+    amount: 55,
+    currencyCode: 'USD',
+    date: '2022-10-01',
+  },
+];
+
+const income = [
+  {
+    title: 'Salary December',
+    amount: 2500,
+    currencyCode: 'USD',
+    date: '2022-12-30',
+  },
+  {
+    title: 'Salary November',
+    amount: 2500,
+    currencyCode: 'USD',
+    date: '2022-11-30',
+  },
+  {
+    title: 'Salary October',
+    amount: 2500,
+    currencyCode: 'USD',
+    date: '2022-10-30',
+  },
+  {
+    title: 'Salary September',
+    amount: 2500,
+    currencyCode: 'USD',
+    date: '2022-09-30',
+  },
+];
+
+async function seed() {
+  const start = performance.now();
+  const expensePromises = expenses.map((expense) => createExpense(expense));
+  const invoicePromises = income.map((income) => createInvoice(income));
+  await Promise.all([...expensePromises, ...invoicePromises]);
+  const end = performance.now();
+  console.log(`🚀 Seeded the database. Done in ${Math.round(end - start)}ms`);
+}
+
+function createExpense(expenseData: typeof expenses[number]) {
+  return db.expense.create({
+    data: {
+      title: expenseData.title,
+      amount: expenseData.amount,
+      currencyCode: expenseData.currencyCode,
+      createdAt: new Date(expenseData.date),
+    },
+  });
+}
+
+function createInvoice(incomeData: typeof income[number]) {
+  return db.invoice.create({
+    data: {
+      title: incomeData.title,
+      amount: incomeData.amount,
+      currencyCode: incomeData.currencyCode,
+      createdAt: new Date(incomeData.date),
+    },
+  });
+}
+
+seed();
