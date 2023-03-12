460ab2e0e9a79e28df549a140fd66d545a09a998
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-03-12 15:44:06 -0700

    init chapter 10 solution

---
+++ 10-working-with-files/bee-rich/solution/app/routes/dashboard/expenses/index.tsx
@@ -0,0 +1,51 @@
+import type { ActionArgs } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { useNavigation } from '@remix-run/react';
+import { Button } from '~/components/buttons';
+import { Form, Input, Textarea } from '~/components/forms';
+import { db } from '~/db.server';
+import { requireUserId } from '~/session.server';
+
+export async function action({ request }: ActionArgs) {
+  const userId = await requireUserId(request);
+  const formData = await request.formData();
+  const title = formData.get('title');
+  const description = formData.get('description');
+  const amount = formData.get('amount');
+  if (typeof title !== 'string' || typeof description !== 'string' || typeof amount !== 'string') {
+    throw Error('something went wrong');
+  }
+  const amountNumber = Number.parseFloat(amount);
+  if (Number.isNaN(amountNumber)) {
+    throw Error('something went wrong');
+  }
+  const expense = await db.expense.create({
+    data: {
+      title,
+      description,
+      amount: amountNumber,
+      currencyCode: 'USD',
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+  return redirect(`/dashboard/expenses/${expense.id}`);
+}
+
+export default function CreateExpensePage() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  return (
+    <Form method="post" action="/dashboard/expenses/?index">
+      <Input label="Title:" type="text" name="title" placeholder="Dinner for Two" required />
+      <Textarea label="Description:" name="description" />
+      <Input label="Amount (in USD):" type="number" defaultValue={0} name="amount" required />
+      <Button type="submit" disabled={isSubmitting} isPrimary>
+        {isSubmitting ? 'Creating...' : 'Create'}
+      </Button>
+    </Form>
+  );
+}
---
+++ 10-working-with-files/bee-rich/solution/tailwind.config.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FBBF24',
+        primaryAccent: '#F59E0B',
+        accent: '#222222',
+        secondary: '#C4C4C4',
+        secondaryAccent: '#9CA3AF',
+        background: '#F5F5F5',
+        backgroundPrimary: '#FFFFFF',
+        darkPrimary: '#803805',
+        darkPrimaryAccent: '#8A3C05',
+        darkAccent: '#DEBF79',
+        darkSecondary: '#444D5A',
+        darkSecondaryAccent: '#4C505C',
+        darkBackground: '#222222',
+        darkBackgroundPrimary: '#1F1F1F',
+        darkShadow: '#000000',
+        text: '#222222',
+        darkText: '#F5F5F5',
+      },
+      fontFamily: {
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
