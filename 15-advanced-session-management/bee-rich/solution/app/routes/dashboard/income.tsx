737923446df1aac95f835e147f1cfb289950262e
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-06-24 19:25:44 -0700

    implement pagination

--- 15-advanced-session-management/bee-rich/solution/app/routes/dashboard/income.tsx
+++ 15-advanced-session-management/bee-rich/solution/app/routes/dashboard/income.tsx
@@ -1,38 +1,55 @@
 import { clsx } from 'clsx';
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Form, Outlet, useLoaderData, useLocation, useParams, useNavigation } from '@remix-run/react';
+import { useNavigation, Outlet, useLoaderData, useParams, Form, useLocation, useSearchParams } from '@remix-run/react';
+import type { Prisma } from '@prisma/client';
 import { ListLinkItem } from '~/components/links';
 import { H1 } from '~/components/headings';
 import { db } from '~/db.server';
 import { useRef } from 'react';
 import { SearchInput } from '~/components/forms';
 import { requireUserId } from '~/session.server';
+import { Button } from '~/components/buttons';
+
+const PAGE_SIZE = 10;
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
   const url = new URL(request.url);
   const searchString = url.searchParams.get('q');
-  const invoices = await db.invoice.findMany({
-    orderBy: {
-      createdAt: 'desc',
+  const pageNumberString = url.searchParams.get('page');
+  const pageNumber = pageNumberString ? Number(pageNumberString) : 1;
+
+  const where: Prisma.InvoiceWhereInput = {
+    userId,
+    title: {
+      contains: searchString ? searchString : '',
     },
-    where: {
-      userId,
-      title: {
-        contains: searchString ? searchString : '',
-      },
-    },
-  });
-  return json(invoices);
+  };
+  const [count, invoices] = await Promise.all([
+    db.invoice.count({ where }),
+    db.invoice.findMany({
+      orderBy: { createdAt: 'desc' },
+      take: PAGE_SIZE,
+      skip: (pageNumber - 1) * PAGE_SIZE,
+      where,
+    }),
+  ]);
+  return json({ count, invoices });
 }
 
 export default function IncomePage() {
   const navigation = useNavigation();
-  const invoices = useLoaderData<typeof loader>();
+  const { count, invoices } = useLoaderData<typeof loader>();
   const { id } = useParams();
   const location = useLocation();
   const ref = useRef<HTMLFormElement>(null);
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get('q') || '';
+  const pageNumber = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
+
+  const isOnFirstPage = pageNumber === 1;
+  const showPagination = count > PAGE_SIZE || !isOnFirstPage;
   return (
     <div className="w-full">
       <H1>Your income</H1>
@@ -40,7 +57,8 @@ export default function IncomePage() {
         <section className="lg:p-8 w-full lg:max-w-2xl">
           <h2 className="sr-only">All your income</h2>
           <Form ref={ref} method="get" action={location.pathname}>
-            <SearchInput name="q" type="search" label="Search by title" formRef={ref} />
+            <input type="hidden" name="page" value={1} />
+            <SearchInput name="q" type="search" label="Search by title" formRef={ref} defaultValue={searchQuery} />
           </Form>
           <ul className="flex flex-col">
             {invoices.map((invoice) => (
@@ -67,6 +85,17 @@ export default function IncomePage() {
               </ListLinkItem>
             ))}
           </ul>
+          {showPagination && (
+            <Form method="get" action="/dashboard/income" className="flex justify-between">
+              <input type="hidden" name="q" value={searchQuery} />
+              <Button type="submit" name="page" value={pageNumber - 1} disabled={pageNumber === 1}>
+                Previous
+              </Button>
+              <Button type="submit" name="page" value={pageNumber + 1} disabled={count <= pageNumber * PAGE_SIZE}>
+                Next
+              </Button>
+            </Form>
+          )}
         </section>
         <section className={clsx('lg:p-8 w-full', navigation.state === 'loading' && 'motion-safe:animate-pulse')}>
           <Outlet />
