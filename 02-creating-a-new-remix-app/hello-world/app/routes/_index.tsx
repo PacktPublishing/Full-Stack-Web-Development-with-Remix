ad935f7f77abec03dc6aa67eb22183637e3d921f
Andre Landgraf <andre.timo.landgraf@gmail.com>
2023-08-31 19:39:33 -0700

    new chapter 02 hello world app

--- 02-creating-a-new-remix-app/hello-world/app/routes/_index.tsx
+++ 02-creating-a-new-remix-app/hello-world/app/routes/_index.tsx
@@ -1,4 +1,12 @@
+import type { V2_MetaFunction } from "@remix-run/node";
 import { useEffect, useState } from "react";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
 
 export default function Index() {
   const [userAgent, setUserAgent] = useState('the World');
